7c5333341a6324e30c284f740022994e78035f6e
Samuel Xavier <samuelxavi@gmail.com>
2021-04-17 19:03:21 -0300

    assets bootstrap 4

--- src/ImagesBundle/Controller/DefaultController.php
+++ src/ImagesBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ImagesBundle:Default:index.html.twig');
+        return $this->render('ImagesBundle:Default:home.html.twig');
     }
 }
